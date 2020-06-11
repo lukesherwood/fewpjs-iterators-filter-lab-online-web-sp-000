eb03a6b7d77e655f664def8a1d5e2e71bbad1666
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-11 21:18:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string){
+  return array.filter(driver => {
+    return driver.toUpperCase() == string.toUpperCase();
+  })
+}
+
+function fuzzyMatch(array, string){
+  return array.filter(driver => {
+    return driver[0] == string[0];
+  })
+}
+
+function matchName (array, string){
+  return array.filter(driver => {
+    return driver.name == string;
+  })
+}
